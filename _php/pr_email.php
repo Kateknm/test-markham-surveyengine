91b27954f512630768e39c8c8747dd0fd63a639f
Kateknm <31115258+Kateknm@users.noreply.github.com>
2017-11-26 17:44:58 -0800

    email

--- _php/pr_email.php
+++ _php/pr_email.php
@@ -1,45 +1,47 @@
 <?php
+
+//Load composer's autoloader
 require 'vendor/autoload.php';
+
 // ++++ Change: Renamed from email to pr_email distinguish from other emails that might be added later ++++
 function emailToken($emailLink, $email){
-$emailLink = "http:" . $emailLink;	
-$emailLinkStr = '<a href"' . $emailLink . '">'. $emailLink . '</a>';
+	$emailLink = "http:" . $emailLink;	
+	$emailLinkStr = '<a href"' . $emailLink . '">'. $emailLink . '</a>';
 
+	<?php
+	$from = new SendGrid\Email(null, "surveyadmin@mga.edu");
+	// subject
+	$subject = 'MGA Password Reset Request';
+	// receipient email from forgotpassword page
+	$to = new SendGrid\Email(null, $email);
 
-<?php
-$from = new SendGrid\Email(null, "surveyadmin@mga.edu");
-// subject
-$subject = 'MGA Password Reset Request';
-// receipient email from forgotpassword page
-$to = new SendGrid\Email(null, $email);
+	// message
+	$message = '
+	<html>
+	<head>
+	  <title>MGA Survey Password Reset Request</title>
+	</head>
+	<body>
+	  <p>We received your request to reset your password.<br/>
+		 To reset your password, click on the button below (or copy/paste the URL into your browser). <br/> 
+	  </p>
+	 <p>'.
+	 $emailLinkStr
+	 .'</p>
+	</body>
+	</html>
+	';
 
-// message
-$message = '
-<html>
-<head>
-  <title>MGA Survey Password Reset Request</title>
-</head>
-<body>
-  <p>We received your request to reset your password.<br/>
-	 To reset your password, click on the button below (or copy/paste the URL into your browser). <br/> 
-  </p>
- <p>'.
- $emailLinkStr
- .'</p>
-</body>
-</html>
-';
+	$content = new SendGrid\Content("text/html", $message);
 
-$content = new SendGrid\Content("text/html", $message);
+	$mail = new SendGrid\Mail($from, $subject, $to, $content);
 
-$mail = new SendGrid\Mail($from, $subject, $to, $content);
+	$apiKey = getenv('SENDGRID_API_KEY');
+	$sg = new \SendGrid($apiKey);
 
-$apiKey = getenv('MGA_SENDGRID_API_KEY');
-$sg = new \SendGrid($apiKey);
-
-$response = $sg->client->mail()->send()->post($mail);
-echo $response->statusCode();
-echo $response->headers();
-echo $response->body();
+	$response = $sg->client->mail()->send()->post($mail);
+	echo $response->statusCode();
+	echo $response->headers();
+	echo $response->body();
 }
 ?>
